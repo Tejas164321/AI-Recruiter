f639cb9cfe9769f34898806222f8bd384906c107
Tejas Patil <tejas2382004@gmail.com>
2025-06-16 17:12:18 +0000

    also improve the terms of services and privacy policy

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -13,6 +13,8 @@ export default function TermsPage() {
 
       <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use ResumeRank AI if you do not agree to take all of the terms and conditions stated on this page.</p>
 
+      <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: &quot;Client&quot;, &quot;You&quot; and &quot;Your&quot; refers to you, the person log on this website and compliant to the Company’s terms and conditions. &quot;The Company&quot;, &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot; and &quot;Us&quot;, refers to our Company. &quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot;, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+
       <h2 className="text-2xl font-semibold mt-6 mb-3">Cookies</h2>
       <p>We employ the use of cookies. By accessing ResumeRank AI, you agreed to use cookies in agreement with the ResumeRank AI&apos;s Privacy Policy.</p>
       <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
@@ -29,8 +31,40 @@ export default function TermsPage() {
 
       <h2 className="text-2xl font-semibold mt-6 mb-3">User Comments</h2>
       <p>This Agreement shall begin on the date hereof.</p>
-      <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. ResumeRank AI does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of ResumeRank AI, its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions.</p>
-      {/* Add more sections as needed */}
+      <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. ResumeRank AI does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of ResumeRank AI, its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, ResumeRank AI shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
+      <p>ResumeRank AI reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
+      <p>You warrant and represent that:</p>
+      <ul>
+        <li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
+        <li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+        <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy</li>
+        <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+      </ul>
+      <p>You hereby grant ResumeRank AI a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
+
+      <h2 className="text-2xl font-semibold mt-6 mb-3">Hyperlinking to our Content</h2>
+      <p>The following organizations may link to our Website without prior written approval:</p>
+      <ul>
+        <li>Government agencies;</li>
+        <li>Search engines;</li>
+        <li>News organizations;</li>
+        <li>Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
+        <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
+      </ul>
+      <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.</p>
+
+      <h2 className="text-2xl font-semibold mt-6 mb-3">iFrames</h2>
+      <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
+
+      <h2 className="text-2xl font-semibold mt-6 mb-3">Content Liability</h2>
+      <p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
+
+      <h2 className="text-2xl font-semibold mt-6 mb-3">Reservation of Rights</h2>
+      <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+
+      <h2 className="text-2xl font-semibold mt-6 mb-3">Removal of links from our website</h2>
+      <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
+      <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
 
       <h2 className="text-2xl font-semibold mt-6 mb-3">Disclaimer</h2>
       <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
@@ -43,7 +77,7 @@ export default function TermsPage() {
       <p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
       <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
 
-      <p className="mt-8">This is a placeholder Terms of Service. Please replace this with your own comprehensive terms.</p>
+      <p className="mt-8"><strong>Please Note:</strong> This is a placeholder Terms of Service document. You should replace this with a comprehensive policy tailored to your specific services and legal requirements. Consult with a legal professional for assistance.</p>
     </div>
   );
 }
